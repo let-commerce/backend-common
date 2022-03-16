9cf0f2be39b0ffb5126c43b87faac260b35722a1
tzvikaperelmuter <tzvikaper@gmail.com>
2022-03-16 18:19:10 +0200

    add encoder decoder

---
+++ utils/encoders/encoder.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/let-commerce/backend-common/env"
+	"github.com/speps/go-hashids"
+)
+
+var (
+	salt = env.GetEnvVar("SALT")
+	//prime      = env.GetEnvVar("PRIME")
+	//modInverse = env.GetEnvVar("MOD_INVERSE")
+	//random     = env.GetEnvVar("PRIME")
+)
+
+func EncodeHashId(id int) string {
+	// read here: https://hashids.org/go/
+	hd := hashids.NewData()
+	hd.Salt = salt
+	hd.MinLength = 4
+	h, _ := hashids.NewWithData(hd)
+	result, _ := h.Encode([]int{id})
+	return result
+}
+
+func DecodeHashId(str string) int {
+	hd := hashids.NewData()
+	hd.Salt = salt
+	hd.MinLength = 4
+	h, _ := hashids.NewWithData(hd)
+	numbers, _ := h.DecodeWithError(str)
+	return numbers[0]
+}
+
+//func EncodeOptimus(id uint64) uint64 {
+//	// read here: https://github.com/pjebs/optimus-go
+//	o := optimus.New(prime, modInverse, random)
+//	return o.Encode(id)
+//}
+//
+//func DecodeOptimus(encodedId uint64) uint64 {
+//	o := optimus.New(prime, modInverse, random)
+//	return o.Decode(encodedId)
+//}
