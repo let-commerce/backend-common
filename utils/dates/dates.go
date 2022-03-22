89607f818aaa96adbc3f137cba85e4417086255b
tzvikaperelmuter <tzvikaper@gmail.com>
2022-03-22 18:15:05 +0200

    change date format

--- utils/dates/dates.go
+++ utils/dates/dates.go
@@ -4,7 +4,7 @@ import "time"
 
 const dateFormat = "1/2/2006"
 
-// ParseDate create a new Date from the passed string in format "M/D/YYYY"
+// ParseDate create a Date from the passed string in format "M/D/YYYY"
 func ParseDate(date string) (time.Time, error) {
 	return time.Parse(dateFormat, date)
 }
