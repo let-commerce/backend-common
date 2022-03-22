04543b0c3b3244a22968e8ffb8bd6b85aeaab709
tzvikaperelmuter <tzvikaper@gmail.com>
2022-03-22 17:25:27 +0200

    change date format

--- utils/dates/dates.go
+++ utils/dates/dates.go
@@ -2,9 +2,9 @@ package dates
 
 import "time"
 
-const dateFormat = "05-15-2021"
+const dateFormat = "01-02-2006"
 
-// ParseDate create a new Date from the passed string in format "05-15-2021"
+// ParseDate create a new Date from the passed string in format "MM-DD-YYYY"
 func ParseDate(date string) (time.Time, error) {
 	return time.Parse(dateFormat, date)
 }
