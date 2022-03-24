2f2911c05ecdca8d1f268ba3dc31a67339b3fff1
tzvikaperelmuter <tzvikaper@gmail.com>
2022-03-25 00:53:20 +0200

    add firebase auth

---
+++ auth/firebase.go
@@ -0,0 +1,107 @@
+package auth
+
+import (
+	"context"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/api/option"
+	"gorm.io/gorm"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var (
+	FirebaseAuthClient      *auth.Client
+	UserIdToConsumerIdCache map[string]int
+	TokenCache              *cache.Cache // Keeps token stored for 20 minutes
+)
+
+func SetupFirebase(accountKeyPath string) *auth.Client {
+	serviceAccountKeyFilePath, err := filepath.Abs(accountKeyPath)
+	if err != nil {
+		log.Panicf("Unable to load serviceAccountKeys.json file: %v", err)
+	}
+	opt := option.WithCredentialsFile(serviceAccountKeyFilePath)
+	//Firebase admin SDK initialization
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Panicf("Firebase Admin SDK load error: %v", err)
+	}
+	//Firebase Auth
+	auth, err := app.Auth(context.Background())
+	if err != nil {
+		log.Panicf("Firebase Auth load error: %v", err)
+	}
+	TokenCache = cache.New(20*time.Minute, 20*time.Minute)
+
+	return auth
+}
+
+// AuthMiddleware : to verify all authorized operations
+func AuthMiddleware(ctx *gin.Context) {
+	firebaseAuth := ctx.MustGet("firebaseAuth").(*auth.Client)
+	authorizationToken := ctx.GetHeader("Authorization")
+	idToken := strings.TrimSpace(strings.Replace(authorizationToken, "Bearer", "", 1))
+	uid, done := getUid(ctx, idToken, firebaseAuth)
+	if uid == "" || done {
+		return
+	}
+
+	ctx.Set("FIREBASE_USER_UID", uid)
+	consumerId, success := tryExtractConsumerIdFromUid(ctx, firebaseAuth, uid)
+	if !success {
+		return
+	}
+	ctx.Set("AUTHENTICATED_CONSUMER_ID", consumerId)
+	ctx.Next()
+}
+
+func getUid(ctx *gin.Context, idToken string, firebaseAuth *auth.Client) (string, bool) {
+	if idToken == "" {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "No id token found for this request"})
+		ctx.Abort()
+		return "", true
+	}
+	if uid, found := TokenCache.Get(idToken); found {
+		return uid.(string), false
+	}
+	//verify token
+	token, err := firebaseAuth.VerifyIDToken(context.Background(), idToken)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("Token not verified, err: %v", err)})
+		ctx.Abort()
+		return "", true
+	}
+	TokenCache.Set(idToken, token.UID, cache.DefaultExpiration)
+	UserIdToConsumerIdCache = map[string]int{}
+	return token.UID, false
+}
+
+func tryExtractConsumerIdFromUid(ctx *gin.Context, firebaseAuth *auth.Client, uid string) (int, bool) {
+	if consumerId, ok := UserIdToConsumerIdCache[uid]; ok {
+		return consumerId, true
+	}
+	userRecord, err2 := firebaseAuth.GetUser(context.Background(), uid)
+	if err2 != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("User record not found: %v", err2)})
+		ctx.Abort()
+		return 0, false
+	}
+	ctx.Set("FIREBASE_USER_EMAIL", userRecord.Email)
+
+	var result int
+	err2 = ctx.MustGet("DB").(*gorm.DB).Raw("SELECT id FROM consumers.consumers WHERE email = ?", userRecord.Email).Scan(&result).Error
+	if err2 != nil || result == 0 {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Consumer record not found for email: %v, err: %v", userRecord.Email, err2)})
+		ctx.Abort()
+		return 0, false
+	}
+	UserIdToConsumerIdCache[uid] = result
+	return result, true
+}
--- go.mod
+++ go.mod
@@ -16,13 +16,20 @@ require (
 )
 
 require (
+	cloud.google.com/go v0.100.2 // indirect
 	cloud.google.com/go/compute v1.3.0 // indirect
+	cloud.google.com/go/firestore v1.6.1 // indirect
+	cloud.google.com/go/iam v0.3.0 // indirect
+	cloud.google.com/go/storage v1.10.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	firebase.google.com/go/v4 v4.7.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.11.0 // indirect
@@ -40,20 +47,22 @@ require (
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
-	google.golang.org/api v0.68.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.70.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220207164111-0872dc986b00 // indirect
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
 	google.golang.org/grpc v1.44.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
