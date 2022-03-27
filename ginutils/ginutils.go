67baea3c35eeaf58279be166644bdea94c9261bf
tzvikaperelmuter <tzvikaper@gmail.com>
2022-03-27 21:43:41 +0300

    new gin utils

--- ginutils/ginutils.go
+++ ginutils/ginutils.go
@@ -12,41 +12,41 @@ import (
 	"time"
 )
 
-var (
+type GinUtils struct {
 	ctx *gin.Context
-)
+}
 
-func Init(ginCtx *gin.Context) {
-	ctx = ginCtx
+func Init(ginCtx *gin.Context) *GinUtils {
+	return &GinUtils{ginCtx}
 }
 
 // GetIntParam method binds new int Param from ctx and return http.StatusBadRequest if it couldn't parse
-func GetIntParam(paramName string) (int, error) {
-	paramVal := ctx.Params.ByName(paramName)
+func (utils *GinUtils) GetIntParam(paramName string) (int, error) {
+	paramVal := utils.ctx.Params.ByName(paramName)
 	intParam, err := strconv.Atoi(paramVal)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to int (value = %v)", paramName, paramVal))
+		utils.ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to int (value = %v)", paramName, paramVal))
 	}
 	return intParam, err
 }
 
 // GetUIntParam method binds new uint Param from ctx and return http.StatusBadRequest if it couldn't parse
-func GetUIntParam(paramName string) (uint, error) {
-	paramVal := ctx.Params.ByName(paramName)
+func (utils *GinUtils) GetUIntParam(paramName string) (uint, error) {
+	paramVal := utils.ctx.Params.ByName(paramName)
 	intParam, err := strconv.Atoi(paramVal)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to uint (value = %v)", paramName, paramVal))
+		utils.ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to uint (value = %v)", paramName, paramVal))
 	}
 	return uint(intParam), err
 }
 
 // GetIntQuery method binds new int Param from ctx query and return http.StatusBadRequest if it couldn't parse
-func GetIntQuery(paramName string) (int, bool, error) {
-	paramVal, exists := ctx.GetQuery(paramName)
+func (utils *GinUtils) GetIntQuery(paramName string) (int, bool, error) {
+	paramVal, exists := utils.ctx.GetQuery(paramName)
 	if exists && paramVal != "null" {
 		intParam, err := strconv.Atoi(paramVal)
 		if err != nil {
-			ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to int (value = %v)", paramName, paramVal))
+			utils.ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to int (value = %v)", paramName, paramVal))
 		}
 		return intParam, exists, err
 	}
@@ -54,12 +54,12 @@ func GetIntQuery(paramName string) (int, bool, error) {
 }
 
 // GetUIntQuery method binds new uint Param from ctx query and return http.StatusBadRequest if it couldn't parse
-func GetUIntQuery(paramName string) (uint, bool, error) {
-	paramVal, exists := ctx.GetQuery(paramName)
+func (utils *GinUtils) GetUIntQuery(paramName string) (uint, bool, error) {
+	paramVal, exists := utils.ctx.GetQuery(paramName)
 	if exists && paramVal != "null" {
 		intParam, err := strconv.Atoi(paramVal)
 		if err != nil {
-			ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to uint (value = %v)", paramName, paramVal))
+			utils.ctx.JSON(http.StatusBadRequest, response.NewErrorResponseF(errors.WithStack(err), "can't bind param: %v to uint (value = %v)", paramName, paramVal))
 		}
 		return uint(intParam), exists, err
 	}
@@ -67,15 +67,15 @@ func GetUIntQuery(paramName string) (uint, bool, error) {
 }
 
 // GetDateParam method binds date string Param from ctx (in format: "2006-01-15")
-func GetDateParam(paramName string) (time.Time, error) {
-	paramVal := ctx.Params.ByName(paramName)
+func (utils *GinUtils) GetDateParam(paramName string) (time.Time, error) {
+	paramVal := utils.ctx.Params.ByName(paramName)
 	date, err := datetime.ParseDate(paramVal)
 	return date, err
 }
 
 // GetDateQuery method binds date string Param from ctx (in format: "2006-01-15")
-func GetDateQuery(paramName string) (time.Time, bool, error) {
-	paramVal, exists := ctx.GetQuery(paramName)
+func (utils *GinUtils) GetDateQuery(paramName string) (time.Time, bool, error) {
+	paramVal, exists := utils.ctx.GetQuery(paramName)
 	if exists && paramVal != "null" {
 		date, err := datetime.ParseDate(paramVal)
 		return date, exists, err
@@ -84,14 +84,14 @@ func GetDateQuery(paramName string) (time.Time, bool, error) {
 }
 
 // GetStringParam method binds new string Param from ctx
-func GetStringParam(paramName string) string {
-	paramVal := ctx.Params.ByName(paramName)
+func (utils *GinUtils) GetStringParam(paramName string) string {
+	paramVal := utils.ctx.Params.ByName(paramName)
 	return paramVal
 }
 
 // GetStringQuery method binds new string Query from ctx
-func GetStringQuery(paramName string) (string, bool) {
-	paramVal, exists := ctx.GetQuery(paramName)
+func (utils *GinUtils) GetStringQuery(paramName string) (string, bool) {
+	paramVal, exists := utils.ctx.GetQuery(paramName)
 	if paramVal == "null" {
 		return "", false
 	}
@@ -99,8 +99,8 @@ func GetStringQuery(paramName string) (string, bool) {
 }
 
 // GetBoolQuery method binds bool string Param from ctx query (example: "true")
-func GetBoolQuery(paramName string) (bool, bool, error) {
-	paramVal, exists := ctx.GetQuery(paramName)
+func (utils *GinUtils) GetBoolQuery(paramName string) (bool, bool, error) {
+	paramVal, exists := utils.ctx.GetQuery(paramName)
 	if exists && paramVal != "null" {
 		result, err := strconv.ParseBool(paramVal)
 		return result, exists, err
@@ -109,7 +109,7 @@ func GetBoolQuery(paramName string) (bool, bool, error) {
 }
 
 // BindDTO method binds new DTO from ctx body
-func BindDTO[T any](dto T) (T, error) {
+func BindDTO[T any](ctx *gin.Context, dto T) (T, error) {
 	err := ctx.Bind(&dto)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(fmt.Sprintf("Got error while binding dto: %v", dto), errors.WithStack(err)))
@@ -118,11 +118,11 @@ func BindDTO[T any](dto T) (T, error) {
 }
 
 // BindMap method binds new map from ctx body
-func BindMap() (map[string]interface{}, error) {
+func (utils *GinUtils) BindMap() (map[string]interface{}, error) {
 	var dto map[string]interface{}
-	err := ctx.Bind(&dto)
+	err := utils.ctx.Bind(&dto)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse("Got error while binding map", errors.WithStack(err)))
+		utils.ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse("Got error while binding map", errors.WithStack(err)))
 	}
 	return dto, err
 }
@@ -132,7 +132,7 @@ type IValidatable interface {
 }
 
 // BindAndValidateDTO method binds new DTO from ctx body
-func BindAndValidateDTO[T IValidatable](dto T) (T, error) {
+func BindAndValidateDTO[T IValidatable](ctx *gin.Context, dto T) (T, error) {
 	var null T
 	err := ctx.Bind(&dto)
 	if err != nil {
@@ -147,39 +147,39 @@ func BindAndValidateDTO[T IValidatable](dto T) (T, error) {
 	return dto, err
 }
 
-func ReturnResultOrError(result interface{}, errMessage string, err error) {
+func (utils *GinUtils) ReturnResultOrError(result interface{}, errMessage string, err error) {
 	if err == nil {
-		ctx.JSON(http.StatusOK, result)
+		utils.ctx.JSON(http.StatusOK, result)
 	} else {
-		ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
+		utils.ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
 	}
 }
 
-func ReturnInternalServerError(errMessage string, err error) {
-	ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
+func (utils *GinUtils) ReturnInternalServerError(errMessage string, err error) {
+	utils.ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
 }
 
-func ReturnBadRequestError(errMessage string, err error) {
-	ctx.JSON(http.StatusBadRequest, response.NewErrorResponse(errMessage, errors.WithStack(err)))
+func (utils *GinUtils) ReturnBadRequestError(errMessage string, err error) {
+	utils.ctx.JSON(http.StatusBadRequest, response.NewErrorResponse(errMessage, errors.WithStack(err)))
 }
 
-func ReturnMessageResponseOrError(message string, errMessage string, err error) {
+func (utils *GinUtils) ReturnMessageResponseOrError(message string, errMessage string, err error) {
 	if err == nil {
-		ctx.JSON(http.StatusOK, response.Response{Message: message})
+		utils.ctx.JSON(http.StatusOK, response.Response{Message: message})
 	} else {
-		ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
+		utils.ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
 	}
 }
 
-func ReturnMessageResponseWithIdOrError(message string, id uint, errMessage string, err error) {
+func (utils *GinUtils) ReturnMessageResponseWithIdOrError(message string, id uint, errMessage string, err error) {
 	if err == nil {
-		ctx.JSON(http.StatusOK, response.Response{Message: message, ID: id})
+		utils.ctx.JSON(http.StatusOK, response.Response{Message: message, ID: id})
 	} else {
-		ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
+		utils.ctx.JSON(http.StatusInternalServerError, response.NewErrorResponse(errMessage, errors.WithStack(err)))
 	}
 }
 
-func CopyDTO[T any](to T, from interface{}, ignoreEmpty bool) (result T, err error) {
+func CopyDTO[T any](ctx *gin.Context, to T, from interface{}, ignoreEmpty bool) (result T, err error) {
 	var null T
 	if ignoreEmpty {
 		err = copier.CopyWithOption(&to, from, copier.Option{IgnoreEmpty: true})
