74a837cbc7f93eecfb341290e49de514c688db00
tzvikaperelmuter <tzvikaper@gmail.com>
2022-04-05 17:06:58 +0300

    add get uid

--- auth/auth_utils.go
+++ auth/auth_utils.go
@@ -18,6 +18,14 @@ func GetIsAdmin(ctx *gin.Context) bool {
 	return false
 }
 
+func GetAuthenticatedUid(ctx *gin.Context) string {
+	uid, exists := ctx.Get("FIREBASE_USER_UID")
+	if exists {
+		return uid.(string)
+	}
+	return ""
+}
+
 func GetIsGuest(ctx *gin.Context) bool {
 	isGuest, exists := ctx.Get("IS_GUEST")
 	if exists {
