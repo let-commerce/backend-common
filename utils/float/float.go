67c4bc71adc5bdb0e2317fef3404c729291ff45c
tzvikaperelmuter <tzvikaper@gmail.com>
2022-04-13 17:21:47 +0300

    fix float utils

--- utils/float/float.go
+++ utils/float/float.go
@@ -4,10 +4,10 @@ import "math"
 
 func ToTwoPoints32[T float32 | float64](num T) float32 {
 	num64 := float64(num)
-	return float32(math.Floor(num64*100)) / 100
+	return float32(math.Round(num64*100)) / 100
 }
 
 func ToTwoPoints64[T float32 | float64](num T) float64 {
 	num64 := float64(num)
-	return math.Floor(num64*100) / 100
+	return math.Round(num64*100) / 100
 }
